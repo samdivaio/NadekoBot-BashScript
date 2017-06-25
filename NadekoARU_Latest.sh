da114a8fc7aee1ff9b8e40a1a282d9653f3b390d
samvaio <samdivaio@icloud.com>
2017-06-25 16:51:57 +0530

    fix

--- NadekoARU_Latest.sh
+++ NadekoARU_Latest.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Running NadekoBot with auto restart and updating to latest build!"
 sleep 5s
-while :; do cd NadekoBot && dotnet restore && dotnet build --configuration Release && cd - && cd NadekoBot/src/NadekoBot && dotnet run -c Release -- {0} {1} {2} && cd - && curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/1.4/nadeko_installer_latest.sh | sh; sleep 5s; done
+while :; do cd NadekoBot && dotnet restore && dotnet build --configuration Release && cd - && cd NadekoBot/src/NadekoBot && dotnet run -c Release && cd - && wget -N https://cdn.discordapp.com/attachments/287982972664020994/325603867431534603/1.4-new.sh && bash 1.4-new.sh; sleep 5s; done
 echo ""
 echo "That didn't work? Please report in #NadekoLog Discord Server."
 sleep 3s
