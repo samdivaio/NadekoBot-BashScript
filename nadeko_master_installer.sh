733f5b10b20ad238dffb991b807980f801ca3c37
samvaio <samdivaio@icloud.com>
2020-05-28 21:59:31 +0530

    Credentials Update
    Changes according to JSON Explanations guide.

--- nadeko_master_installer.sh
+++ nadeko_master_installer.sh
@@ -68,57 +68,91 @@ cd "$root/NadekoBot/src/NadekoBot"
 mv credentials.json credentials.json.old
 
 echo Please enter your bot client ID:
+echo ""
 read clientid
+echo ""
 echo Alright saved \'$clientid\' as your client ID.
 echo ""
 echo "----------"
 echo ""
 
 echo Please enter your bot token \(It is not bot secret, it should be ~59 characters long.\):
+echo ""
 read token
+echo ""
 echo Alright saved \'$token\' as your bot\'s token.
 echo ""
 echo "----------"
 echo ""
 
 echo Please enter your own ID \(Refer to the guide, it will be bot\'s owner ID.\):
+echo ""
 read ownerid
+echo ""
 echo Alright saved \'$ownerid\' as owner\'s ID.
 echo ""
 echo "----------"
 echo ""
 
 echo Please enter Google API key \(Refer to the guide.\):
+echo ""
 read googleapi
+echo ""
 echo Alright saved \'$googleapi\' as your bot\'s Google API Key.
 echo ""
 echo "----------"
 echo ""
 
-echo Please enter LoL API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
-read lolapikey
-echo Alright saved \'$lolapikey\'
+echo -e "Please enter Mashape Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
 echo ""
-echo "----------"
-echo ""
-
-echo Please enter Mashape Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
 read mashapekey
-echo Alright saved \'$mashapekey\'
+echo ""
+echo Alright saved \'$mashapekey\' as your bot\'s Mashape Key.
 echo ""
 echo "----------"
 echo ""
 
-echo Please enter Osu API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+echo -e "Please enter OSU API Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
+echo ""
 read osu
-echo Alright saved \'$osu\'
+echo ""
+echo Alright saved \'$osu\' as your bot\'s OSU API Key.
 echo ""
 echo "----------"
 echo ""
 
-echo Please enter Cleverbot Api Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+echo -e "Please enter Cleverbot API Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
+echo ""
 read cleverbot
-echo Alright saved \'$cleverbot\'
+echo ""
+echo Alright saved \'$cleverbot\' as your bot\'s Cleverbot API Key.
+echo ""
+echo "----------"
+echo ""
+
+echo -e "Please enter Twitch Client ID or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
+echo ""
+read twitchcid
+echo ""
+echo Alright saved \'$twitchcid\' as your bot\'s Twitch Client ID.
+echo ""
+echo "----------"
+echo ""
+
+echo -e "Please enter Location IQ Api Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
+echo ""
+read locationiqapi
+echo ""
+echo Alright saved \'$locationiqapi\' as your bot\'s Location IQ API Key.
+echo ""
+echo "----------"
+echo ""
+
+echo -e "Please enter Timezone DB Api Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
+echo ""
+read timedbapi
+echo ""
+echo Alright saved \'$timedbapi\' as your bot\'s Timezone DB API Key.
 echo ""
 echo "----------"
 echo ""
@@ -129,11 +163,13 @@ echo "{
   \"OwnerIds\": [
     $ownerid
   ],
-  \"LoLApiKey\": \"$lolapikey\",
   \"GoogleApiKey\": \"$googleapi\",
   \"MashapeKey\": \"$mashapekey\",
   \"OsuApiKey\": \"$osu\",
   \"CleverbotApiKey\": \"$cleverbot\",
+  \"TwitchClientId\": \"$twitchcid\",
+  \"LocationIqApiKey\": \"$locationiqapi\",
+  \"TimezoneDbApiKey\": \"$timedbapi\",
   \"Db\": null,
   \"TotalShards\": 1
 }" | cat - >> credentials.json
